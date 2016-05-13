e19616f048c1015c21e077079591e7b0f45aa40b
Yunlin <yunlinz@gmail.com>
2016-05-13 07:47:52 -0400

    corrected typos

--- num2proj.py
+++ num2proj.py
@@ -18,7 +18,7 @@ u0.t = 0
 
 u1 = interpolate(u0, V)
 Tmax = 2
-nT = 100
+nT = 20
 dt = Tmax / nT
 dt = 0.001
 a = u * v * dx + dt * inner(nabla_grad(u), nabla_grad(v)) * dx
@@ -30,7 +30,6 @@ A = assemble(a)
 u = Function(V)
 T = 0
 Y = np.zeros((u.vector().array().shape[0], nT))
-print u.vector().array().shape
 for i in range(nT):
     T += dt
     b = assemble(L)
@@ -38,15 +37,11 @@ for i in range(nT):
     solve(A, u.vector(), b)
     u1.assign(u)
     Y[:, i] = u.vector().array()
-    plot(u, interactive=False)
 
-U, S, V = np.linalg.svd(Y)
-
-plt.semilogy(S)
-plt.show()
+u, s, v = np.linalg.svd(Y, full_matrices=False)
 
 nPC = 5 # only want 5 PC's
-Phi = np.dot(U, S)[:, nPC]
+Phi = np.dot(u, np.diag(s))[:, :nPC]
 
 # now construct the our time stepping matrices
 Phi_i = Function(V)
@@ -57,19 +52,21 @@ for i in range(nPC):
         Phi_i.vector().set_local(Phi[:, i])
         Phi_j.vector().set_local(Phi[:, j])
         # using assemble as a numerical integrator
-        K[i,j] -= assemble(inner(grad(Phi_i), grad(Phi_j)) * dx)
-        k[j,i] -= assemble(inner(grad(Phi_i), grad(Phi_j)) * dx)
+        integral =  assemble(inner(grad(Phi_i), grad(Phi_j)) * dx)
+
+        K[i,j] += integral
+        K[j,i] += integral
 
 K += np.eye(nPC)
 
 
 # now to do the time stepping, we just solve U^k = K * U^k-1
-u_init = np.dot(np.dot(interpolate(u0, V).vector().array(),U),
-                np.inv(S)).T # map this into PCA space to get our initial U^0
+u_init = np.dot(np.dot(interpolate(u0, V).vector().array(), u),
+                np.linalg.inv(np.diag(s))).T[:nPC] # map this into PCA space to get our initial U^0
 
 Y2 = np.zeros((nPC, nT))
 Y2[:, 0] = u_init
 for i in range(1, nT):
-    Y2[:, i] = np.dot(np.inv(K), Y2[:, i - 1])
+    Y2[:, i] = np.dot(np.linalg.inv(K), Y2[:, i - 1])
 
 
